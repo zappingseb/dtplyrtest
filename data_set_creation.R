3ad1a9df5bde1edaf215479b588e825c0124ca97
zappingseb <sebastian@mail-wolf.de>
2019-11-16 18:47:38 +0000

    first version

---
+++ data_set_creation.R
@@ -0,0 +1,12 @@
+data_set_creation <- function(n) {
+  words = vapply(1:15, FUN = function(x) paste(sample(LETTERS, 15, TRUE), collapse = ""), FUN.VALUE = character(1))
+  data.frame(
+    word = sample(words, n, TRUE),
+    letter = sample(LETTERS, n, TRUE),
+    id = 1:n,
+    numeric_int = sample(1:100, n, TRUE),
+    numeric_double = sample(seq(from = 0, to = 2, by = 1/(10^6)), n, TRUE)
+  )
+  
+}
+
