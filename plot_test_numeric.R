540027a6ef52d5fa679006ab09bf212e7fc04619
zappingseb <sebastian@mail-wolf.de>
2019-11-17 22:52:50 +0000

    updated plotting tab

--- plot_test_numeric.R
+++ plot_test_numeric.R
@@ -1,19 +1,24 @@
 plot_test_numeric <- function(n = 100, iterations = 100, fun = test_dataset_numeric) {
   
+  plot_data <- plot_data_fun(n, iternations, fun)
+  plot_data <- plot_data %>% filter(time <= 2 * mean(time))
+  ggplot2::ggplot(data.frame(plot_data), ggplot2::aes(x = call, y = time)) +
+    ggplot2::geom_boxplot() +
+    ggplot2::coord_flip() +
+    ggplot2::ggtitle(paste("Dataset has", n, "rows", "| OS:", Sys.info()["sysname"]))
+  
+}
+
+plot_data_fun <- function(n = 100, iterations = 100, fun = test_dataset_numeric) {
   my_data <- data_set_creation(n)
   
   x <- fun(my_data, iterations = iterations)
   
   plot_data <- data.frame(
     call = matrix(vapply(x$expression %>% as.character(), function(x) rep(x, iterations), character(iterations)), ncol = 1),
-    time = as.numeric(do.call("c", x$time))
+    time = as.numeric(do.call("c", x$time)),
+    n = n
   )
   
-  plot_data <- plot_data %>% filter(time <= 2 * mean(time))
-  
-  ggplot2::ggplot(data.frame(plot_data), ggplot2::aes(x = call, y = time)) +
-    ggplot2::geom_boxplot() +
-    ggplot2::coord_flip() +
-    ggplot2::ggtitle(paste("Dataset has", n, "rows", "| OS:", Sys.info()["sysname"]))
-  
+  return(plot_data)
 }
--- run.R
+++ run.R
@@ -1,22 +1,59 @@
 install.packages(c("bench", "dtplyr", "dplyr", "ggplot2"))
 library(dtplyr)
 library(dplyr)
+library(data.table)
+library(bench)
 
 source("data_set_creation.R")
 source("test_dataset_numeric.R")
 source("test_dataset_char.R")
 source("plot_test_numeric.R")
 
-plot_test_numeric(n = 100, iterations = 1000)
+plot_data_num <- data.frame(
+  call = c(),
+  time = c(),
+  n = c()
+)
 
-plot_test_numeric(n = 10 ^ 5, iterations = 1000)
+for (n in c(100, 1000, 10E3, 10E4, 10E5, 10E6)){
+  print(n)
+  plot_data_num <- rbind(plot_data_num,
+                         plot_data_fun(n, 1000, test_dataset_numeric)
+                         )
+}
 
-plot_test_numeric(n = 10 ^ 6, iterations = 1000)
+data_gg <- plot_data_num %>% group_by(call, n) %>%
+  summarise(mean = mean(time), ymax = quantile(time, probs = c(0.75)), ymin = quantile(time, probs=(0.25)))
 
-plot_test_numeric(n = 10 ^ 7, iterations = 500)
+ggplot2::ggplot(data_gg, ggplot2::aes(x=n, y=mean, colour=call)) + 
+  ggplot2::geom_errorbar(ggplot2::aes(ymin=ymin, ymax=ymax), width=.1) +
+  ggplot2::geom_line() +
+  ggplot2::geom_point() +
+  ggplot2::xlab("Dataset size") +
+  ggplot2::scale_y_continuous(trans = "log10") +
+  ggplot2::scale_x_continuous(trans = "log10")
 
-plot_test_numeric(n = 100, iterations = 1000, fun = test_dataset_char)
-
-plot_test_numeric(n = 10 ^ 5, iterations = 1000, fun = test_dataset_char)
-
-plot_test_numeric(n = 10 ^ 7, iterations = 1000)
+plot_data_char <- data.frame(
+  call = c(),
+  time = c(),
+  n = c()
+)
+for (n in c(100, 1000, 10E3, 10E4, 10E5, 10E6)){
+  print(n)
+  plot_data_char <- rbind(plot_data_char,
+                         plot_data_fun(n, 1000, test_dataset_char)
+                         )
+}
+data_gg <- plot_data_char %>% group_by(call, n) %>%
+  summarise(mean = mean(time), ymax = quantile(time, probs = c(0.75)), ymin = quantile(time, probs=(0.25)))
+data_gg$call <- factor(as.character(data_gg$call), c("mutate_char_dt(DT)",
+                                               "data.table::as.data.table(ds) %>% mutate_char_dt()",
+                                               "ds %>% mutate_char()",
+                                               "ds_lazy %>% mutate_char() %>% as_tibble()"))
+ggplot2::ggplot(data_gg, ggplot2::aes(x=n, y=mean, colour=call)) + 
+  ggplot2::geom_errorbar(ggplot2::aes(ymin=ymin, ymax=ymax), width=.1) +
+  ggplot2::geom_line() +
+  ggplot2::geom_point() +
+  ggplot2::xlab("Dataset size") +
+  ggplot2::scale_y_continuous(trans = "log10") +
+  ggplot2::scale_x_continuous(trans = "log10")
